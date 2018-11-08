9db4159bac9d0c1e46d5e941515f523aaf77489f
nic547 <nic547@users.noreply.github.com>
2018-11-08 10:10:53 +0100

    Editing Notes is a bit nicer

--- app/src/main/java/ch/beerpro/presentation/details/privateNote/PrivateNoteFragment.java
+++ app/src/main/java/ch/beerpro/presentation/details/privateNote/PrivateNoteFragment.java
@@ -2,23 +2,15 @@ package ch.beerpro.presentation.details.privateNote;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
 
 import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.ViewModelProviders;
 import ch.beerpro.R;
 import ch.beerpro.presentation.details.DetailsActivity;
 import ch.beerpro.domain.models.PrivateNote;
-import ch.beerpro.presentation.details.DetailsViewModel;
-
 public class PrivateNoteFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -48,7 +40,8 @@ public class PrivateNoteFragment extends DialogFragment {
     }
     public void updateNote(PrivateNote note) {
         EditText et = getDialog().findViewById(R.id.PrivateNoteET);
-        et.setText(note.getNote());
-
+        String noteText = note.getNote();
+        et.setText(noteText);
+        et.setSelection(noteText.length());
     }
 }
